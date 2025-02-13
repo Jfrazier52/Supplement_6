f3c4999e40f9bd622b25d73dd558724c1fd308f6
jamar frazier <jamarfrazier52@tgmail.com>
2025-02-13 17:33:06 -0500

    Chore: Made a abstract class case for Shape3D

--- Supplement_5/Supplement5.cs
+++ Supplement_5/Supplement5.cs
@@ -1,6 +1,38 @@
-﻿namespace Supplement_5;
+﻿using Supplement_5;
 
-public class Supplement_5
+namespace Supplement_5
 {
+     public class Cube : Shape3D
+    {
+        private double side;
 
+        public Cube(double side)
+        {
+            this.side = side;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (side <= 0)
+            {
+                throw new ArgumentException("Side length must be greater than 0.");
+            }
+        }
+
+        public override double GetVolume()
+        {
+            return side * side * side;
+        }
+
+        public override double GetSurfaceArea()
+        {
+            return 6 * side * side;
+        }
+
+        public override string Dump()
+        {
+            return $"Cube: Surface Area = {GetSurfaceArea()}, Volume = {GetVolume()}";
+        }
+    }
 }

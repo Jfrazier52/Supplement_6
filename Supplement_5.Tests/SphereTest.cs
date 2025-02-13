2a3ffb52965b5a111f084d5ff7aaaa9f886b9d30
jamar frazier <jamarfrazier52@tgmail.com>
2025-02-13 18:04:20 -0500

    Chore: Added unit test for sphere: volume, and surfacearea

---
+++ Supplement_5.Tests/SphereTest.cs
@@ -0,0 +1,74 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Supplement_5;
+
+namespace Supplement_5.Tests
+{
+    [TestClass]
+    public class SphereTests
+    {
+        [TestMethod]
+        public void Sphere_CalculatesVolumeCorrectly()
+        {
+            // Arrange
+            double radius = 3;
+            Sphere sphere = new Sphere(radius);
+            double expectedVolume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+
+            // Act
+            double volume = sphere.GetVolume();
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expectedVolume, volume, 0.001);
+        }
+
+        [TestMethod]
+        public void Sphere_CalculatesSurfaceAreaCorrectly()
+        {
+            // Arrange
+            double radius = 3;
+            Sphere sphere = new Sphere(radius);
+            double expectedSurfaceArea = 4 * Math.PI * radius * radius;
+
+            // Act
+            double surfaceArea = sphere.GetSurfaceArea();
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expectedSurfaceArea, surfaceArea, 0.001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Sphere_InvalidRadius_ThrowsException()
+        {
+            // Arrange & Act
+            Sphere sphere = new Sphere(0); // invalid radius
+        }
+    }
+
+    internal class Sphere
+    {
+        private int v;
+        private double radius;
+
+        public Sphere(int v)
+        {
+            this.v = v;
+        }
+
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+
+        internal double GetSurfaceArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
